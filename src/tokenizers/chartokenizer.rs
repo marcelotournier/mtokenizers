a7b1b22ee8a5bc1bb766eea4fee1fb03814b5623
marcelotournier <dr.tournier@gmail.com>
2023-06-04 19:47:07 -0400

    tokenizer encoder working

---
+++ src/tokenizers/chartokenizer.rs
@@ -0,0 +1,35 @@
+use std::cmp::Ordering;
+use encoding::{Encoding, EncoderTrap};
+use encoding::all::ISO_8859_1;
+
+
+pub fn encode(input: &str, vector_size: usize) -> Vec<u8> {
+    let encoded = ISO_8859_1
+    .encode(input, EncoderTrap::Strict)
+    .expect("Tokenizer Encoder Error when converting characters to bytes");
+    
+    match encoded.len().cmp(&vector_size) {
+        Ordering::Less => {
+            let padding_size = vector_size - encoded.len();
+            let zeros: Vec<u8>  = vec![0; padding_size];
+            [encoded, zeros].concat()
+        },
+        Ordering::Greater => encoded[..vector_size].to_vec(),
+        Ordering::Equal => encoded,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encode() {
+        let output = vec![72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33];
+        let padded_zeros: Vec<u8> = vec![0; 2];
+
+        assert_eq!(encode("Hello, world!", 10), output[..10]);
+        assert_eq!(encode("Hello, world!", 13), output);
+        assert_eq!(encode("Hello, world!", 15), [output, padded_zeros].concat());
+    }
+}
